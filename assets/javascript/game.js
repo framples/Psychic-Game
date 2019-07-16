3c5d60fbf53019787bbe0c498f612ad2b41e9ae0
Illio, the Great <pwfcoding@gmail.com>
2019-07-16 11:27:39 -0500

    initial functions...idk

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,29 @@
 var alphabet = ["a",
-                "b",
-                "c",
-                "d",
-                "e",
-                "f",
-                "g",
-                "h",
-                "i",
-                "j",
-                "k",
-                "l",
-                "m",
-                "n",
-                "o",
-                "p",
-                "q",
-                "r",
-                "s",
-                "t",
-                "u",
-                "v",
-                "w",
-                "x",
-                "y",
-                "z"];
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"];
 
 var wins = 0;
 var losses = 0;
@@ -32,6 +32,17 @@ var guesses = 9;
 var guessesSoFar = [];
 var psychicLetter;
 
+var newLetter = function () {
+    psychicLetter = alphabet[math.floor(Math.random() * alphabet.length)];
+};
+
+var soFar = function () {
+    document.getElementById("guesses").innerHTML = "Guesses so far: " + guessesSoFar.join(",");
+};
+
+var guessesLeft = function () {
+    document.getElementById("left").innterHTML = "Guesses left: " + left;
+}
 
 
 
@@ -59,4 +70,4 @@ var psychicLetter;
 
 
 
-]
+
